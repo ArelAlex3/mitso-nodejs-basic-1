a39227d225514d6d85280be51bee2e1dfcfd6fa2
ArelAlex3 <arelalex03.06@gmail.com>
2025-03-19 11:31:44 +0300

    create.js and env

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,21 @@
 const parseEnv = () => {
-    // Write your code here 
+    let MITSO_Name1 = "dzmitry";
+    let MITSO_Name2 = "KOlya";
+    let mahonch = "maks"
+    const envVariables = process.env;
+        .filter(key => key.startsWith('MITSO_'))
+        .reduce((acc, key) => {
+            acc[key] = envVariables[key];
+            return acc;
+        }, {});
+
+    // Формируем строку в требуемом формате
+    const result = Object.entries(mitsoVariables)
+        .map(([key, value]) => `${key}=${value}`)
+        .join('; ');
+
+    // Выводим результат на консоль
+    console.log(result);
 };
 
 parseEnv();
--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,19 @@
+import { promises as fs } from 'fs';
+import path from 'path';
+
 const create = async () => {
-    // Write your code here 
+    try {
+        const filePath = path.join(process.cwd(), './files/newFile.txt'); // Указывает путь к файлу
+        const content = 'This is a new file created by the create function!';
+        await fs.writeFile(filePath, content, { flag: 'wx' }); // Создание файла
+        console.log('File created successfully');
+    } catch (err) {
+        if (err.code === 'EEXIST') {
+            console.error('File already exists');
+        } else {
+            console.error('An error occurred:', err.message);
+        }
+    }
 };
 
 await create();
